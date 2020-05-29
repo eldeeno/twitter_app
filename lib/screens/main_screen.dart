7a8b0d83bd30b7c39c585c71e3b7ae467b62dfdc
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2020-05-29 12:27:45 +0100

    layout of main twitter page set

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -7,80 +7,88 @@ class MainScreenScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 18.0,
-                  backgroundColor: Colors.white,
-                  backgroundImage: AssetImage('images/logo.png'),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 13.0,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage('images/user.png'),
+                    ),
+                    CircleAvatar(
+                      radius: 15.0,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage('images/logo.png'),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: FaIcon(
+                        FontAwesomeIcons.home,
+                        color: Color(0xff1C9AE6),
+                        size: 15.0,
+                      ),
+                    ),
+                  ],
                 ),
-                CircleAvatar(
-                  radius: 18.0,
-                  backgroundColor: Colors.white,
-                  backgroundImage: AssetImage('images/logo.png'),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: FaIcon(
-                    FontAwesomeIcons.home,
-                    color: Color(0xff1C9AE6),
-                    size: 20.0,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-          Divider(),
-          Expanded(
-            flex: 6,
-            child: Column(),
-          ),
-          Expanded(
-            flex: 2,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                IconButton(
-                  onPressed: () {},
-                  icon: FaIcon(
-                    FontAwesomeIcons.home,
-                    color: Color(0xff1C9AE6),
-                    size: 20.0,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: FaIcon(
-                    FontAwesomeIcons.search,
-                    color: Color(0xff1C9AE6),
-                    size: 20.0,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: FaIcon(
-                    FontAwesomeIcons.bell,
-                    color: Color(0xff1C9AE6),
-                    size: 20.0,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: FaIcon(
-                    FontAwesomeIcons.envelope,
-                    color: Color(0xff1C9AE6),
-                    size: 20.0,
-                  ),
-                )
-              ],
+            Divider(),
+            Expanded(
+              flex: 8,
+              child: Column(
+                children: <Widget>[],
+              ),
             ),
-          )
-        ],
+            Divider(),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      FontAwesomeIcons.home,
+                      color: Color(0xff1C9AE6),
+                      size: 20.0,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      FontAwesomeIcons.search,
+                      color: Color(0xff1C9AE6),
+                      size: 20.0,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      FontAwesomeIcons.bell,
+                      color: Color(0xff1C9AE6),
+                      size: 20.0,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      FontAwesomeIcons.envelope,
+                      color: Color(0xff1C9AE6),
+                      size: 20.0,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
